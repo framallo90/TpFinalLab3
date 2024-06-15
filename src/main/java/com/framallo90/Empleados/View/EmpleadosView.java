982906d56a3bcd62049dae56723de685976772d3
piragna1 <gonzalovarelaalagna@gmail.com>
2024-06-15 15:00:39 -0300

    ## Implementación de la clase EmpleadosView
    Este commit introduce la clase `EmpleadosView`, la cual proporciona funcionalidades para la interacción con el usuario en la gestión de empleados.
    
    **Principales cambios:**
    
    * Se crea la clase `EmpleadosView` con métodos para:
        * Generar un nuevo objeto Empleados a partir de la información ingresada por el usuario.
        * Permitir al usuario seleccionar el tipo de empleado (vendedor o administrador).
    
    **Mejoras:**
    
    * Se centraliza la interacción con el usuario para la gestión de empleados en una sola clase.
    * Se utiliza la consola para la interacción con el usuario.
    * Se implementan mecanismos de validación de datos para garantizar la integridad de la información ingresada.
    
    **Notas:**
    
    * Esta clase es una vista para la gestión de empleados. No interactúa directamente con el modelo de datos.
    
    **Referencias:**
    
    * [Consola](https://github.com/framallo90/empleados/tree/master/src/main/java/com/framallo90/consola)
    * [Entidad Empleados](https://github.com/framallo90/empleados/blob/master/src/main/java/com/framallo90/Empleados/Model/Entity/Empleados.java)

--- src/main/java/com/framallo90/Empleados/View/EmpleadosView.java
+++ src/main/java/com/framallo90/Empleados/View/EmpleadosView.java
@@ -1,4 +1,56 @@
+/**
+ * Clase que proporciona métodos para la interacción con el usuario para la gestión de empleados.
+ *
+ * @author Framballo90
+ * @since v1.0
+ */
 package com.framallo90.Empleados.View;
 
+import com.framallo90.Empleados.Model.Entity.Empleados;
+import com.framallo90.consola.Consola;
+
 public class EmpleadosView {
+
+    /**
+     * Genera un nuevo objeto Empleados a partir de la información ingresada por el usuario.
+     *
+     * @return Un nuevo objeto Empleados con los datos ingresados.
+     */
+    public Empleados generarEmpleado() {
+        String nombre, apellido, username, password, tipo;
+        Integer dni;
+
+        nombre = Consola.ingresarXString("nombre");
+        apellido = Consola.ingresarXString("apellido");
+        dni = Consola.ingresarXInteger("dni");
+        Consola.limpiarBuffer();
+        username = Consola.ingresarXString("username");
+        password = Consola.ingresarXString("password");
+        tipo = this.generarTipo();
+
+        return new Empleados(nombre, apellido, dni, 0, username, password, tipo);
+    }
+
+    /**
+     * Permite al usuario seleccionar el tipo de empleado (vendedor o administrador) y lo devuelve como cadena.
+     *
+     * @return El tipo de empleado seleccionado por el usuario.
+     */
+    public String generarTipo() {
+        String tipo;
+
+        while (true) {
+            tipo = Consola.ingresarXString("""
+                    \n  tipo de usuario:
+                    vendedor
+                    administrador\n""");
+
+            if (tipo.equalsIgnoreCase("administrador") ||
+                    tipo.equalsIgnoreCase("vendedor")) {
+                return tipo;
+            } else {
+                System.out.println("ingresar un dato válido");
+            }
+        }
+    }
 }
