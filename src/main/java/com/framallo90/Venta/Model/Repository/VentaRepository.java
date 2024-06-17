116740297e4e41cd60d8c1cb100a4bcffd72cc57
piragna1 <gonzalovarelaalagna@gmail.com>
2024-06-17 20:01:36 -0300

    VentaRepository add Desarrollo de clase
    VentaRepository add VentaRepository Desarrollo de la clase VentaRepository con sus respectivos atributos y constructor.
    VentaRepository add Funciones para gestionar un archivo JSON de ventas (métodos para tomar las ventas de un archivo y para guardarlas en el mismo).
    VentaRepository add funciones CRUD Agrego las funciones básicas de CRUD para agregar, leer, actualizar y remover instancias de ventas.
    VentaRepository add find() Agrego función para devolver una instancia existente de una venta.
    VentaRepository add cambioEmpleados() Agrego función para cambiar el empleado de una instancia existente de una venta.
    VentaRepository add cambioComprador() Agrego función para cambiar el comprador de una instancia existente de una venta.
    VentaRepository add cambioAutomovil() Agrego función para cambiar el automovil de una instancia existente de una venta.
    VentaRepository add camioFecha() Agrego función para cambiar la fecha de una instancia existente de una venta.
    VentaRepository add cambioMetodoDePago() Agrego función para cambiar la forma de pago de una instancia existente de una venta.
    VentaRepository add getMap() Agrego función que devuelve la colección del repositorio.

--- src/main/java/com/framallo90/Venta/Model/Repository/VentaRepository.java
+++ src/main/java/com/framallo90/Venta/Model/Repository/VentaRepository.java
@@ -1,4 +1,119 @@
 package com.framallo90.Venta.Model.Repository;
 
-public class VentaRepository {
+import com.framallo90.Automovil.Model.Entity.Automovil;
+import com.framallo90.Comprador.Model.Entity.Comprador;
+import com.framallo90.Empleados.Model.Entity.Empleados;
+import com.framallo90.Excepciones.InvalidIdNotFound;
+import com.framallo90.Interfaces.IRepository;
+import com.framallo90.MetodoDePago.Model.Entity.MetodoDePago;
+import com.framallo90.Venta.Model.Entity.Venta;
+import com.framallo90.consola.Consola;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class VentaRepository  implements IRepository<Venta,Integer> {
+    private Map<Integer, Venta> map;
+    private static final String PATH_VENTAS = "src/main/resources/ventas.json";
+    private final Gson gson = new Gson();
+
+    public VentaRepository() {
+        this.loadVentas();
+    }
+
+    public void loadVentas(){
+        try(FileReader fileReader = new FileReader(PATH_VENTAS)){
+            Type listType = new TypeToken<Map<Integer,Venta>>(){}.getType();
+            this.map = gson.fromJson(fileReader,listType);
+            if (this.map == null) this.map = new HashMap<>();
+        } catch (FileNotFoundException e) {
+            this.map = new HashMap<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveVentas(){
+        try(FileWriter fileWriter = new FileWriter(PATH_VENTAS)){
+            gson.toJson(this.map,fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Map<Integer, Venta> getMap() {
+        return map;
+    }
+
+    @Override
+    public void add(Venta object) {
+        this.map.put(object.getIdVenta(),object);
+        this.saveVentas();
+    }
+
+    @Override
+    public void remove(Integer integer) throws Exception {
+        Venta remove = this.map.remove(integer);
+        if (remove == null )throw new InvalidIdNotFound("No se ha encontrado una venta con id " + integer + ".");
+        else Consola.soutString("Venta removida correctamente.");
+        this.saveVentas();
+    }
+
+    @Override
+    public void update(Integer integer) throws Exception {
+        Venta update = this.find(integer);
+        if (update != null ){
+            //terminar
+        }
+        else throw new InvalidIdNotFound("No se ha encontrado una venta de id " + integer + ".");
+    }
+
+    @Override
+    public Venta find(Integer integer) {
+        return this.map.get(integer);
+    }
+
+    public void cambioEmpleados(Integer idVenta, Empleados nuevo){
+        Venta buscar = this.find(idVenta);
+        if (buscar != null) {
+            buscar.setEmpleados(nuevo);
+            this.saveVentas();
+        }
+    }
+
+    public void cambioComprador(Integer idVenta, Comprador nuevo){
+        Venta buscar = this.find(idVenta);
+        if (buscar != null) {
+            buscar.setComprador(nuevo);
+            this.saveVentas();
+        }
+    }
+
+    public void cambioAutomovil(Integer idVenta, Automovil nuevo){
+        Venta buscar = this.find(idVenta);
+        if (buscar != null )
+            buscar.setAutomovil(nuevo);
+        this.saveVentas();
+    }
+
+    public void cambioFecha(Integer idVenta, LocalDate nueva){
+        Venta buscar = this.find(idVenta);
+        if (buscar != null )
+            buscar.setFecha(nueva);
+        this.saveVentas();
+    }
+    public void cambioMetodoDePago(Integer idVenta, MetodoDePago nuevo){
+        Venta buscar = this.find(idVenta);
+        if (buscar != null )
+            buscar.setMetodoDePago(nuevo);
+        this.saveVentas();
+    }
+
 }
